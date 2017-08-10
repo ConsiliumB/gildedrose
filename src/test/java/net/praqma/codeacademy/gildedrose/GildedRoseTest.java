7ef695a544f649a85b60200c3da48daac8241334
ConsiliumB <ConsiliumB@users.noreply.github.com>
2017-08-10 08:37:53 +0200

    Added test for normal item degredation

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -8,11 +8,17 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        //Hah
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
-
+    
+    @Test
+    public void normal_item_normal_quality_degredation() {
+        Item[] items = new Item("Conjured Mana Cake", 3, 6);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(6, app.items[0].quality);
+    }
 }
